47b0ff3d884b2e414203134d365637c2b7dde9b3
Tiko <blacksheep493@gmail.com>
2019-03-17 17:13:45 -0400

    social media icons added

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,4 +1,16 @@
-$(function(e){
-  var loader = document.getElementById("loader");
-  console.log(loader);
-})
+window.onload = function() {
+  var lastRun = localStorage.getItem("last_Run_Time");  
+  var now = Date.now();
+  console.log('lastRun: ', lastRun);
+
+  localStorage.setItem("last_Run_Time", now);
+
+  $(function(e){
+    var loader = document.getElementById("loader");
+    $("div.loader").toggleClass('disabled');
+  })
+
+
+
+
+}
